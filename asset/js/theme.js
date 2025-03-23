36a32805e5ff56fb5d3caee8aab2081946a23760
LaTaevia Berry <lataevia.berry@gmail.com>
2025-03-23 13:12:20 -0500

    working theme skeleton

---
+++ asset/js/theme.js
@@ -0,0 +1,13 @@
+/**
+ * Rosenwald Fund Collection - JavaScript Functions
+ */
+
+(function() {
+    'use strict';
+    
+    // Add mobile navigation toggle
+    document.addEventListener('DOMContentLoaded', function() {
+        // You can add any theme-specific JavaScript functionality here
+        console.log('Rosenwald Fund Collection theme initialized');
+    });
+})();
